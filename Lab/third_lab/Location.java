51828c5a4f967c9a3390704b607d8d306db023fe
PressFK <alexsapr2003@gmail.com>
2022-11-05 04:23:17 +0300

    Some refactors in 3 lab

--- Lab/third_lab/Location.java
+++ Lab/third_lab/Location.java
@@ -25,15 +25,14 @@ public class Location
         this(0, 0);
     }
 
+    @Override
     public boolean equals(Object obj) {
         if (obj == null) {
             return false;
         }
-
         if (obj instanceof Location) {
             // Cast other object to Point3d type, then compare.
             Location other = (Location) obj;
-
             if (xCoord == other.xCoord &&
                     yCoord == other.yCoord) {
                 return true;
@@ -43,10 +42,10 @@ public class Location
         return false;
     }
 
+    @Override
     public int hashCode() {
-        int result = 19; // Some prime value
-
-        // Use another prime value to combine
+        int result = 19; // prime value
+        // another prime value to combine
         result = 17 * result + (xCoord * 11);
         result = 23 * result + (yCoord * 13);
 
